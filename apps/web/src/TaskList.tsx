dd9db9c3981e9907cb562cd0cd5fda98f3f0a5a6
hugivar <github.oxes7@simplelogin.com>
2023-07-18 20:39:03 -0400

    Update process.env

--- apps/web/src/TaskList.tsx
+++ apps/web/src/TaskList.tsx
@@ -4,7 +4,7 @@ import { TodoistApi } from '@doist/todoist-api-typescript'
 
 const addService = (service: string) => {
     if (service === 'todoist') {
-        fetch(`https://todoist.com/oauth/authorize?client_id=${import.meta.env.TODOIST_CLIENT_ID}&scope=data:read_write&state=1234`, {
+        fetch(`https://todoist.com/oauth/authorize?client_id=${process.env.TODOIST_CLIENT_ID}&scope=data:read_write&state=1234`, {
             "headers": {
                 "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                 "accept-language": "en-US,en;q=0.9",
