ffbaaa41ccf6985eec60211aca550a2141c71f10
hugivar <github.oxes7@simplelogin.com>
2023-07-21 13:48:03 -0400

    Update todoist redirect

--- apps/web/pages/api/todoist-redirect.js
+++ apps/web/pages/api/todoist-redirect.js
@@ -11,15 +11,12 @@ export default function handler(req, res) {
         url: `https://todoist.com/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}&redirect_uri=https://tali.so`
     }).then((response) => {
         const accessToken = response.data.access_token;
-        console.log('todoist-redirect line:14', accessToken);
-        setCookie(res, 'Next.js', 'api-middleware!', { path: '/', maxAge: 2592000 })
         // Return the `set-cookie` header so we can display it in the browser and show that it works!
         // res.end()
         res.redirect(`/?todoistAccessToken=${accessToken}`);
         // res.send(accessToken)
         // res.writeHead(302, { Location: `localhost:3002?$accessToken=${accessToken}` }).end();
     }).catch((err) => {
-        console.log(err);
-        res.status(500);
+        res.send(err);
     });
 }
