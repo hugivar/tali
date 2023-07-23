0c83ac9d86c49a434f8a0f378a73cbe71ff4444f
hugivar <github.oxes7@simplelogin.com>
2023-07-22 22:11:09 -0400

    Switch linear redirect api to use URL encoded post

--- apps/web/pages/api/linear-redirect.js
+++ apps/web/pages/api/linear-redirect.js
@@ -5,17 +5,18 @@ const clientSecret = process.env.LINEAR_CLIENT_SECRET
 
 export default function handler(req, res) {
     const requestToken = req.query.code;
-    axios({
-        method: 'post',
-        url: `https://api.linear.app/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}&redirect_uri=https://tali.so&grant_type=authorization_code`
-    }).then((response) => {
-        const accessToken = response.data.access_token;
-        // Return the `set-cookie` header so we can display it in the browser and show that it works!
-        // res.end()
-        res.redirect(`/?linearAccessToken=${accessToken}`);
-        // res.send(accessToken)
-        // res.writeHead(302, { Location: `localhost:3002?$accessToken=${accessToken}` }).end();
-    }).catch((err) => {
-        res.send(err);
-    });
+    const params = new URLSearchParams();
+    params.append('client_id', clientID);
+    params.append('client_secret', clientSecret);
+    params.append('code', requestToken);
+    params.append('redirect_uri', 'https://tali.so');
+    params.append('grant_type', 'authorization_code');
+    axios.post('https://api.linear.app/oauth/token', params)
+        .then((response) => {
+            const accessToken = response.data.access_token;
+
+            res.redirect(`/?linearAccessToken=${accessToken}`);
+        }).catch((err) => {
+            res.send(err);
+        });
 }
